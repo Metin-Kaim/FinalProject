586f63df9c4d4e977fee17b4caa017b07306d014
Metin Kaim <metinkaim0@gmail.com>
2022-08-31 14:04:23 +0300

    ProductDetailDto

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,7 +22,7 @@ namespace Business.Concrete
         {
             //iş kodları
             //Yetkisi var mı?
-
+            
             return _productDal.GetAll();
         }
 
@@ -33,6 +34,11 @@ namespace Business.Concrete
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max); 
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
